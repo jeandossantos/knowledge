68df9c878f5fb249cb712d857652772360fddad0
txtdbr <jeanddg@hotmail.com>
2020-01-25 12:06:51 -0300

    morearticles

--- frontend/src/components/article/ArticlesByCategory.vue
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -2,12 +2,20 @@
     <div class="articles-by-category">
         <PageTitle icon="fa fa-folder-o"
             :main="category.name" sub="Categoria" />
-        
+        <ul>
+            <li v-for="article in articles" :key="article.id">
+                {{article.name}}
+            </li>
+        </ul>
+        <div class="load-more">
+            <button v-if="loadMore" class="btn btn-lg btn-outline-primary"
+             @click="getArticles">Carregar Mais Artigos</button>
+        </div>
     </div>
 </template>
 
 <script>
-import { baseApiUrl } from '@/global'
+import { baseApiUrl, showError } from '@/global'
 import axios from 'axios'
 import PageTitle from '../template/PageTitle'
 
@@ -28,12 +36,18 @@ export default {
             axios(url).then(res => this.category = res.data)
         },
         getArticles() {
-           
+             const url = `${baseApiUrl}/categories/${this.category.id}/articles?page=${this.page}`
+            axios(url).then(res => {
+                this.articles = this.articles.concat(res.data)
+                this.page++;
+                if(res.data.length === 0) this.loadMore = false
+            })
         }
     },
     mounted() {
         this.category.id = this.$route.params.id
-        this.getCategory()
+        this.getCategory();
+        this.getArticles()
         
     }
 }
